5fb8dc8a91c8de92319f8436e20b88aabdfdc2f3
Shawn the R0ck <citypw@gmail.com>
2012-10-04 23:36:35 +0800

    add print_usage()

--- http/http_request/http_request.py
+++ http/http_request/http_request.py
@@ -9,10 +9,19 @@ import socket
 import time
 import re
 
+def print_usage():
+        print "Usage: %s TARGET_SERVER TARGET_PORT \"HTTP REQUEST\"\n" % (sys.argv[0])
+
+try:
 #socket.gethostbyname("www.google.com") ?
-HOST = sys.argv[1]
-REQ = sys.argv[2].replace("\\r\\n", "\r\n")
-PORT = 80
+	HOST = sys.argv[1]
+	PORT = int(sys.argv[2])
+
+except:
+	print_usage()
+	quit()
+
+REQ = sys.argv[3].replace("\\r\\n", "\r\n")
 
 #REQ = "HEAD / HTTP/1.1\r\nHost: " + sys.argv[1] + "\r\n\r\n"
 sockfd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
